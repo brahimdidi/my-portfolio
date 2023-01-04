27814cebaea42aec139557f609555038e6984d13
brahimdidi <brahimgb1team@gmail.com>
2023-01-04 16:15:02 +0000

    set up react routers

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react'
 import Intro from './Components/Intro'
 import Navbar from './Components/Navbar'
 import Projects from './Components/Projects'
+import Contact from './Components/Contact'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Intro/>
-    <Projects />
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/projects" element={<Projects />}> </Route>
+        <Route path="/contact" element={<Contact />}> </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
