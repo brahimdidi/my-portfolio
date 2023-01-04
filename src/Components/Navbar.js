3b029af6244926d478414579a59a9e0ee8e15dd2
brahimdidi <brahimgb1team@gmail.com>
2023-01-04 16:14:43 +0000

    set up  the navbar use navLink

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import logo from '../assets/logo.png'
+import './styles/navbar.css'
 
 function Navbar() {
+  let activeStyle = { textDecoration: "underline",textDecorationColor: "red"};
   return (
-    <div>Navbar</div>
+    <header className='header'>
+      <img className='logo' src={logo}/>
+      <ul className='nav'>
+          <NavLink
+            to="projects"
+            className= "nav-link"
+            style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="contact"
+            className='nav-link'
+            style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
+          >
+            Contact me
+          </NavLink>
+      </ul>
+    </header>
   )
 }
 
