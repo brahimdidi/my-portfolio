72b3761b49f071784d2824dde903488cb797a01c
brahimdidi <brahimgb1team@gmail.com>
2023-01-04 15:24:57 +0000

    create react app ,remove unused code , create app components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Intro from './Components/Intro'
+import Navbar from './Components/Navbar'
+import Projects from './Components/Projects'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Intro/>
+    <Projects />
+    </>
+  )
+}
+
+export default App
