298baebd08b9af0cc437b02904d37d164a120723
brahimdidi <brahimgb1team@gmail.com>
2023-01-06 16:25:25 +0000

    add intro paragrapth to Intro component

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Intro() {
   return (
-    <div>Intro</div>
+    <section>
+      <h1 className='title'>Hi, I'm <span className='name'>Brahim</span></h1>
+      <p>Hello and welcome to my portfolio! My name is Brahim and 
+        I am a full stack web developer with experience in React JS, 
+        Ruby on Rails, and PostgreSQL. With a strong foundation in 
+        both front-end and back-end technologies, I am able to create dynamic and 
+        visually appealing web applications that are also efficient and scalable. 
+        I am constantly seeking to learn and stay up-to-date with the latest 
+        web development trends and techniques, and I am excited to apply my skills 
+        and knowledge to create innovative and user-friendly websites and web applications
+      </p>
+    </section>
   )
 }
 
