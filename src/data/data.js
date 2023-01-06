697642246c836b648f5debfe37d4e14d7172c0a6
brahimdidi <brahimgb1team@gmail.com>
2023-01-06 16:26:37 +0000

    create data.js file with app data.

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+const data ={ 
+  projects: [
+    { name: 'Doctor Appointment', link: 'doctorappointment.com', image: 'dc.png', date: '10-2022'},
+    { name: 'Blog App', link: 'blogapp.com', image: 'dc.png', date: '08-2022'},
+    { name: 'Blog App', link: 'blogapp.com', image: 'dc.png', date: '08-2022'},
+  ],
+};
+
+export default data;
+
